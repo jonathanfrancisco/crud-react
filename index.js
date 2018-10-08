3dfcbe6193045ae8bd32a83e3862f4d333340fe7
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-10-08 16:26:15 +0800

    Add GET students /api/students

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const mysql = require('./db/mysql2.js');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/api/students', (req, res) => {
+    
+    mysql.connect()
+    .then((connection) => {
+        connection.query('SELECT * FROM student')
+        .then((results) => {
+            const [rows, fields] = results;
+            connection.end();
+            res.json(rows);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Started listening on port: ', process.env.PORT || 3000);
