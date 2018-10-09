de48a29d655e9f15a9acac34bbef2d83871479fb
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-10-09 11:23:55 +0800

    Add get student by id api route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mysql = require('./db/mysql2.js');
 const app = express();
 app.use(bodyParser.json());
 
+// fetches all students
 app.get('/api/students', (req, res) => {
     
     mysql.connect()
@@ -24,6 +25,24 @@ app.get('/api/students', (req, res) => {
     });
 
 });
+
+// fetches 1 student by id
+app.get('/api/students/:id', (req, res) => {
+    const {id} = req.params;
+    mysql.connect()
+    .then((connection) => {
+        connection.query('SELECT * FROM student WHERE id = ?', [id])
+        .then((results) => {
+            const [rows, fields] = results;
+            res.json(rows[0]);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Started listening on port: ', process.env.PORT || 3000);
